60c491b9ed2d8aed874e23b8035c937642f28e84
jake <jjk848798@gmail.com>
2023-03-09 20:56:33 +0530

    .

---
+++ Heap/CP/c.cpp
@@ -0,0 +1,2 @@
+// Heap is a complete binary tree that means array representation of a binary tree should not contains any spaces (-) in b/w two nodes
+-- - /// 1 3 4 5 - - 4 5 4  ///--it should not be there
